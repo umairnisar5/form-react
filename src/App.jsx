37f83e5442cc4a46e22ee68e72999c701ee950aa
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-24 03:25:33 +0500

    using form with react

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react"
+
+const App = () => {
+    const [name , setName] = useState("");
+    const [fullName ,setFullNmae ] = useState(); 
+
+    // const inputEvent = (Event) =>{
+
+    //     setName(Event.target.value);
+    // };
+
+    // const onSubmit = () => {
+    //     setFullNmae (name);  
+    // };
+ return (
+
+ <>
+<div>
+ <h1>Hello {fullName} </h1>
+ <input type='text' placeholder="Enetr your name"
+  onChange = {inputEvent}
+  value={name} />
+ <button onClick={onSubmit} >Click Me</button>
+
+</div>
+ </>
+    );
+};
+
+export default App;
